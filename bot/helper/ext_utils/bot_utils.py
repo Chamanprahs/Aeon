ab68cc2e6c55985db09d205c956d1d002909d045
Chamanprahs <chamanprash434@gmail.com>
2024-06-02 14:11:33 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -229,7 +229,7 @@ def get_readable_message():
         else:
             msg += f"</p>Size: {download.size()}"
         msg += f"\nElapsed: {get_readable_time(time() - download.message.date.timestamp())}</blockquote>"
-        msg += f"\n<p>/stop_{download.gid()[:8]}</p>\n\n"
+        msg += f"\n<q>/stop_{download.gid()[:8]}</q>\n\n"
     if len(msg) == 0:
         return None, None
     dl_speed = 0
