629a8bfc456703ef513a24c943f1696392befca8
Chamanprahs <chamanprash434@gmail.com>
2024-06-02 12:39:16 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -185,7 +185,7 @@ def progress_bar(pct):
         pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
     cFull = int((p + 5)// 10)
-    p_str = '🟤' * cFull
+    p_str = '🌚' * cFull
     p_str += '🌕' * (10 - cFull)
     return p_str
 
