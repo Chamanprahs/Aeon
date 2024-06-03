141b460faf92234551fddaf4244329f43a73c30d
Chamanprahs <chamanprash434@gmail.com>
2024-06-03 10:41:11 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -180,16 +180,14 @@ async def fetch_user_tds(user_id, force=False):
     return {}
 
 
-def get_progress_bar_string(pct):
-    pct = float(str(pct).strip('%'))
+def progress_bar(pct):
+    if isinstance(pct, str):
+        pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
-    p_str = '■' * cFull
-    if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
-    return f"[{p_str}]"
+    cFull = int((p + 5)// 10)
+    p_str = '🌚' * cFull
+    p_str += '🌝' * (10 - cFull)
+    return p_str
 
 
 def source(self):
