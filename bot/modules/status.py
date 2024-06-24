c131d58fd69c6318724818148d930528c88aa466
Chamanprahs <chamanprash434@gmail.com>
2024-06-24 23:32:13 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -24,7 +24,6 @@ async def mirror_status(_, message):
         msg += 'No downloads are currently in progress.\n'
         msg += f"\n<b>• Bot uptime</b>: {currentTime}"
         msg += f"\n<b>• Free disk space</b>: {free}"
-        msg += f"\n<b>❤️ kabi pyar me mat dubo bhai👍</b>"
 
         reply_message = await sendMessage(message, msg)
         await deleteMessage(message)
